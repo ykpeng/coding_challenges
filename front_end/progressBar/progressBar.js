e841f07b7f51b144c0f17e364508871ea4e40ef7
Yi-Ke Peng <yi.ke.peng@gmail.com>
2016-08-26 00:32:17 -0700

    add progress bar

---
+++ front_end/progressBar/progressBar.js
@@ -0,0 +1,20 @@
+$(()=>{
+  $(".start-button").on("click", startNewBar);
+})
+
+function doSetTimeout(newBar, percent) {
+  if (percent > 100) {
+    return startNewBar();
+  }
+  newBar.css("width", `${percent}%`);
+  setTimeout(function(){
+    percent++
+    doSetTimeout(newBar, percent);
+  }, 100);
+}
+
+function startNewBar() {
+  const newBar = $("<div>").addClass("bar");
+  $(".bars").prepend(newBar);
+  doSetTimeout(newBar, 1);
+}
